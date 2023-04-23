dd5ea615b7f1a59fc79183356085279bb2254415
Brahm V <brahmvanh@gmail.com>
2023-04-22 22:35:30 -0400

    Filled portfolio container.

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className='navbar-nav mx-auto'>
-      <li className='nav-item'>
+    <ul className="navbar-nav mx-auto">
+      <li className="nav-item">
         <a
           href="#home"
           onClick={() = handlePageChange('Home')}
@@ -12,18 +12,18 @@ function NavTabs({ currentPage, handlePageChange }) {
             Home
           </a>
       </li>
-      <li className='nav-item'>
+      <li className="nav-item">
         <a
-        href='#creations'
+        href="#creations"
         onClick={() = handlePageChange('Creations')}
         className={currentPage === 'Creations' ? 'nav-link active' : 'nav-link'}
         >
           Creations
         </a>
       </li>
-      <li className='nav-item'>
+      <li className="nav-item">
         <a
-          href='#Contact'
+          href="#Contact"
           onClick={() = handlePageChange('Contact')}
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
           >
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -5,3 +5,26 @@ import Footer from './Footer';
 import Header from './Header';
 import Creations from './MyCreations';
 import Welcome from './Welcome';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'Creations') {
+      return <Creations />;
+    }
+      return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
